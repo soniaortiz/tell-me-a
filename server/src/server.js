0a23d5dbe0286a388413c602828ee0ff47581128
Sonia <sonia@Sonias-MacBook-Air.local>
2020-08-03 15:14:54 -0700

    add joke endpoint

--- server/src/server.js
+++ server/src/server.js
@@ -5,6 +5,7 @@ const path = require('path')
 const errorHandler = require('errorhandler')
 const app = express()
 const validator = require('express-validator')
+const { json } = require('body-parser')
 
 
 app.use(express.static(path.join(__dirname, '../build/')))
@@ -15,8 +16,27 @@ app.use(errorHandler())
 app.set('port', 8000)
 
 app.get('/', (req, res, next)=>{
-    res.sendStatus(200)
+    res.sendFile(__dirname, '../../public/index.html')
 })
+
+app.get('/joke', (req, res, next)=>{
+    http.get('http://official-joke-api.appspot.com/jokes/programming/ten', (response)=>{
+        let data =''
+        response.on('data', (chunk)=>{
+            data +=chunk
+        })
+        response.on('end', ()=>{
+            console.log(data)
+            res.send(JSON.parse(data))
+
+        })
+    })
+})
+
+app.get('*', (req, res, next)=>{
+    res.sendFile(__dirname, '../../public/index.html')
+})
+
 
 const server = http.createServer(app)
 const boot = ()=>{
