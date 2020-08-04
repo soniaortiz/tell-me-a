3d86a69b4611efe2116e783c4c6aa634cf0adddf
Sonia <sonia@Sonias-MacBook-Air.local>
2020-08-04 00:41:34 -0700

    add concurrently script, home

--- src/App.js
+++ src/App.js
@@ -10,34 +10,18 @@ import {
   Tab,
 } from '@material-ui/core';
 import { Jokes } from './components/Jokes'
+import {Home} from './components/Home'
 function App() {
   return (
     <>
       <BrowserRouter>
 
         <div>
-          <Route exact path="/" component={Jokes} />
+          <Route exact path="/" component={Home} />
           <Route exact path="/joke" component={Jokes} />
         </div>
 
       </BrowserRouter>
-{/* 
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-        </header>
-      </div> */}
     </>
   );
 }
